7689d3d173e0ff21780dbdd7dd9cf014bc35509c
Mustaghees Butt <renaldpirate@gmail.com>
2024-03-21 23:02:54 +0000

    feat(scripts): RoundCube dependencies + nginx default config

--- install/setup.sh
+++ install/setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+fqdn=$1
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Setup keys
+# Setup APT repo keys
 curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
@@ -18,12 +20,15 @@ apt update
 apt upgrade
 apt install nginx \
   postfix dovecot-pop3d dovecot-imapd opendkim opendkim-tools \
-  php8.3-fpm \
+  php8.3-fpm php8.3-mbstring php8.3-mysql php8.3-xml php8.3-intl php8.3-curl php8.3-ldap php8.3-gd php8.3-imagick php8.3-zip \
   mariadb-server
 
 # Copy config files with proper values
+cat $DIR/../templates/main.cf | sed "s/<FQDN>/$fqdn/g" > /etc/postfix/main.cf
+cp $DIR/../templates/nginx.default.conf /etc/nginx/sites-available/default # This will also change the link in /sites-enabled
 
 # Setup RoundCube web mail client
 curl -L -O https://github.com/roundcube/roundcubemail/releases/download/1.6.6/roundcubemail-1.6.6-complete.tar.gz
+mkdir /var/www/roundcube
 tar -xf roundcubemail-1.6.6-complete.tar.gz -C /var/www/roundcube -v --strip-components=1
 rm roundcubemail-1.6.6-complete.tar.gz
